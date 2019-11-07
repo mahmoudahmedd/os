3647ca45a5f180413c3bf0f1560323b22e716121
3olaa <36084394+3olaa@users.noreply.github.com>
2019-11-07 20:41:33 +0200

    Update Network.java

--- 2/Network.java
+++ 2/Network.java
@@ -1,25 +1,30 @@
 package AssignmentPackage;
 
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Network {
     public static void main (String[] args) {
+        Device d = new Device ();
+        Vector<String> v = new Vector<> (); //will contain all devices that need to connect to the router
         int N = 0;
         System.out.println ("What is number of WI-FI Connections?");
         Scanner in = new Scanner (System.in);
         N = in.nextInt ();
-        Router r = new Router (N);
-        Semaphore s = new Semaphore ();
-        Device d = new Device ();
+        Router r = new Router (N); //send N (number of wifi connections) to router constructor
         Scanner in2 = new Scanner (System.in);
         System.out.println ("What is number of devices Clients want to connect?");
         int TC = in2.nextInt ();
         Scanner in3 = new Scanner (System.in);
         String TCLines;
         for (int  i = 0 ; i < TC ; i++){
-            TCLines = in.nextLine ();
-            d.start ();
+            TCLines = in3.nextLine ();
+            String s[] = TCLines.split(" ");
+            d.name.add (s[0]);
+            d.type.add (s[1]);
+            s = null;
         }
+        d.start ();
 
     }
 }
