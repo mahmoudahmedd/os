9822897f4a52737d685296c6a95d72876d727987
3olaa <36084394+3olaa@users.noreply.github.com>
2019-11-07 20:40:36 +0200

    Update Device.java

--- 2/Device.java
+++ 2/Device.java
@@ -1,10 +1,17 @@
 package AssignmentPackage;
 
+import java.util.Vector;
+
 public class Device extends Thread {
+    Vector name = new Vector ();
+    Vector type = new Vector ();
+
+
     Semaphore s = new Semaphore ();
 
     public void run(){
+        System.out.println (name);
+        System.out.println (type);
 
     }
 }
-
