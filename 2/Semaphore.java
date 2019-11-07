b4b199c1c8f1395e768d4acaa6944d22d2998538
3olaa <36084394+3olaa@users.noreply.github.com>
2019-11-07 20:42:57 +0200

    Update Semaphore.java

--- 2/Semaphore.java
+++ 2/Semaphore.java
@@ -2,7 +2,7 @@ package AssignmentPackage;
 
 public class Semaphore {
 
-    protected int value = 0 ;
+    protected int value = 0 ; //value like a counter
 
     protected Semaphore() {
         value = 0;
@@ -11,9 +11,10 @@ public class Semaphore {
     protected Semaphore(int initial) {
         value = initial ;
     }
-
+    // if the value -ve therefore there is a waiting devices ,
+    // if +ve therefore there is an available connection (fe mkan le device ye2dar yod5ol y3mel connect 3latool mn 8er waiting),
+    // if zero therefore there is no waiting devices and all connections occupied (mfesh waiting bs law device tany geh hay wait)
     public synchronized void P() {
-
         value-- ;
         if (value < 0)
             try { wait();}
